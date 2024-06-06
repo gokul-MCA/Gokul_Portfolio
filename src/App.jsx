8e4f9f9f0c8bcd8806f4a00c115e811b7b36ed4d
gokul-MCA <154983453+gokul-MCA@users.noreply.github.com>
2024-06-06 23:02:01 +0530

    "06/06/24 Contact"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Contact from './Components/Contact/contact';
+import Contact from './Components/Contact/Contact';
 import { Footer } from './Components/Footer/Footer';
 import { Intro } from './Components/Intro/intro';
 import { Navbar } from './Components/Navbar/Navbar';
@@ -7,7 +7,7 @@ import Portfolio from './Components/Portfolio/portfolio';
 import { Skill } from './Components/Skills/skill';
 import About from './Components/About/about';
 // import Timeline from './Components/Timeline/Timeline';
-import Work from './Components/Work/Work';
+// import Work from './Components/Work/Work';
 import { motion, useScroll, useSpring } from "framer-motion";
 import Certificate from './Components/Certificate/certificate';
 
